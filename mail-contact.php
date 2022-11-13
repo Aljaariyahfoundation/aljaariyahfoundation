a87505e3f45d75dea17c190c68a0c8bdc26a47fb
Aljaariyahfoundation <alja.fnd@gmail.com>
2022-11-13 15:03:08 -0800

    uploading files

---
+++ mail-contact.php
@@ -0,0 +1,17 @@
+<?php
+
+	$to = "md.shahinfb24@gmail.com"; // this is your Email address
+	$from  = $_POST['email']; // this is the sender's Email address
+	$sender_name = $_POST['name'];
+	$note = $_POST['note'];
+	$phone = $_POST['phone'];
+	$address = $_POST['address'];
+
+	$subject = "Form submission";
+
+	$message = $sender_name . " has send the contact message. His / Her contact subject is " .  $subject . " and his / her phone number is " . $phone . " and his / her address is " . $address . ". He / she wrote the following... ". "\n\n" . $note;
+
+	$headers = 'From: ' . $from;
+	mail($to, $subject, $message, $headers);
+
+?>
